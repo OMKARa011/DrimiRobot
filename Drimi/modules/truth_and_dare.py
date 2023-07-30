4d1724ffea59389fd743cf632338aa717f2b68db
MOCA <116737841+Moca96@users.noreply.github.com>
2023-07-30 14:04:08 +0530

    Drimi

---
+++ Drimi/modules/truth_and_dare.py
@@ -0,0 +1,34 @@
+
+import requests
+
+from Drimi import SUPPORT_CHAT
+from Drimi.events import register as zadkiel
+
+
+@zadkiel(pattern="[/!]dare")
+async def _(asux):
+    try:
+        ak = requests.get("https://api.truthordarebot.xyz/v1/dare").json()
+        results = f"{ak['question']}"
+        return await asux.reply(results)
+    except Exception:
+        await asux.reply(f"ᴇʀʀᴏʀ ʀᴇᴘᴏʀᴛ @{SUPPORT_CHAT}")
+
+
+@zadkiel(pattern="[/!]truth")
+async def _(asux):
+    try:
+        ak = requests.get("https://api.truthordarebot.xyz/v1/truth").json()
+        results = f"{ak['question']}"
+        return await asux.reply(results)
+    except Exception:
+        await asux.reply(f"ᴇʀʀᴏʀ ʀᴇᴘᴏʀᴛ @{SUPPORT_CHAT}")
+
+
+__mod_name__ = "𝐓ʀᴜᴛʜ-Dᴀʀᴇ"
+
+from Drimi.modules.language import gs
+
+
+def get_help(chat):
+    return gs(chat, "td_help")
