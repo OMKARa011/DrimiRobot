832492e3abc707ff45ff1f058fde1e6081ccb07d
ZADKIEL <116737841+Moca96@users.noreply.github.com>
2023-09-08 23:45:23 +0530

    Update karmas.py

--- Drimi/modules/karmas.py
+++ Drimi/modules/karmas.py
@@ -1,11 +1,40 @@
+"""
+MIT License
+
+Copyright (c) 2022 ABISHNOI69
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+# ""DEAR PRO PEOPLE,  DON'T REMOVE & CHANGE THIS LINE
+# TG :- @Abishnoi1M
+#      :- Abishnoi_bots
+#     GITHUB :- Abishnoi69 ""
+
 
 import asyncio
 
 from pyrogram import filters
 
-from Drimi import OWNER_ID
-from Drimi import Zadkiel as zadkiel
-from Drimi.modules.no_sql.karma_db import (
+from Exon import OWNER_ID
+from Exon import Abishnoi as abishnoi
+from Exon.modules.no_sql.karma_db import (
     alpha_to_int,
     get_karma,
     get_karmas,
@@ -23,7 +52,7 @@ karma_positive_group = 3
 karma_negative_group = 4
 
 
-@zadkiel_z.on_message(
+@abishnoi.on_message(
     filters.text
     & filters.group
     & filters.incoming
@@ -166,4 +195,4 @@ async def captcha_state(_, message):
         await karma_off(message.chat.id)
         await message.reply_text("ᴅɪsᴀʙʟᴇᴅ ᴋᴀʀᴍᴀ sʏsᴛᴇᴍ.")
     else:
-        await message.reply_text
+        await message.reply_text(usage)
